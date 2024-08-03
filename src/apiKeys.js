16aa67e468df808b5dabc3607b63275ad26bfa08
hey-maa <112340310+hey-maa@users.noreply.github.com>
2024-08-03 17:42:51 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "96ef90174c80fad9cffed7e07d433f1d",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
